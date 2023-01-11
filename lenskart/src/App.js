9a2420647f165eea22acd3a96a40be36312b541c
Benjamin <benjaminxchhakchhuak@gmail.com>
2023-01-11 14:20:00 +0530

    first commit

---
+++ lenskart/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import "./App.css";
+import newFile from "./newFile";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hey Everyone</h1>
+      <newFile/>
+    </div>
+  );
+}
+
+export default App;
---
+++ lenskart/src/newFile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import  "./newFile.css";
+
+function newFile(){
+    return(
+        <div>
+            <h1>HTML</h1>
+            <h1>HTML</h1>
+            <h1>HTML</h1>
+            <h1>HTML</h1>
+
+        </div>
+    );
+}
+
+export default newFile;
